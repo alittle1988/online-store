0abcb8d1d0fe81b36b2ee46e57693f59f2a88993
Andrew <andylittle88@gmail.com>
2022-09-07 22:41:50 -0700

    added photos

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
-const kitchenWare = [];
+const kitchen = [];
+const cutlery = [];
 const livingArea = [];
 const bathRoom = [];
 const bedRoom = [];
@@ -32,32 +33,41 @@ Description:${this._description}`
 }
 
 
-const blackGoldCutlery = itemFactory('Black and Gold Cutlery', 1.99, 'kitchen', 'Black and gold eating utensil.', './Resources/Images/Kitchen/cutlery/black-gold-cutlery-set.jpeg' );
-const couch = itemFactory('Couch', 399.99, 'Living Room', 'Gray Sectional Couch', './Resources/Images/Livingroom/grayCouch.jpeg' )
-const blackBedFrame = itemFactory('Black Bedframe', 99.99, 'bedroom', 'Black bedframe with headboard', './Resources/Images/Bedroom/bedframes/BlackBedFrame.jpeg' )
+const blackGoldCutlery = itemFactory('Black and Gold Cutlery', 10.99, 'kitchen', 'Black and gold eating utensil.', './Resources/Images/Kitchen/cutlery/black-gold-cutlery-set.jpeg' );
+const couch = itemFactory('Couch', 399.99, 'Living Room', 'Gray Sectional Couch', './Resources/Images/Livingroom/grayCouch.jpeg' );
+const blackBedFrame = itemFactory('Black Bedframe', 99.99, 'bedroom', 'Black bedframe with headboard', './Resources/Images/Bedroom/bedframes/BlackBedFrame.jpeg' );
+const blackCutlery = itemFactory('Black Cutlery Set', 12,99, 'Kitchen', 'All Black cutlery Set', '/Resources/Images/Kitchen/black-cutlery-set.jpeg');
+const blueSilverCutlery = itemFactory('Blue and Silver Cutlery', 12.99, 'Kitchen', 'Black and Silver Cutlery set.', './Resources/Images/Kitchen/blue-silver-cutlery-set.jpeg');
+const graySilverCutlery = itemFactory('Gray and Silver Cutlery set', 12.99, 'Kitchen', 'Gray and Silver Cutlery set', './Resourses/Images/Kitchen/gray-silver-cutlery-set.jpeg');
+const whiteGoldCutlery = itemFactory('White and Gold Cutlery Set', 12.99, 'Kitchen', 'White and Gold Cutlery Set', './Resources/Images/Kitchen/white-gold-cutlery-set.jpeg')
 deals.push(blackGoldCutlery)
 deals.push(couch)
 deals.push(blackBedFrame)
-
+cutlery.push(blackCutlery, blueSilverCutlery, graySilverCutlery, whiteGoldCutlery,blackGoldCutlery)
+kitchen.push(cutlery)
 
 
 
 // assigning deal location and info
-const dealCards = document.querySelectorAll('.deal-card');
+const dealCards = document.querySelectorAll('.deals');
 
+const setCardFunc = function(Array) {
 
-for(let i = 0; i < dealCards.length; i++) {
-    const card = dealCards[i].children
-    let dealImg = card[0]
-    dealImg.src = deals[i]._imgSrc
-    dealImg.alt = deals[i]._description
-    let cardBody = card[1].children
-    cardBody[0].textContent = deals[i]._name
-    cardBody[1].textContent = deals[i]._price
-    cardBody[2].textContent = deals[i]._description
-    
-    
+    for(let i = 0; i < Array.length; i++) {
+        const card = Array[i].children
+        let dealImg = card[0]
+        dealImg.src = deals[i]._imgSrc
+        dealImg.alt = deals[i]._description
+        let cardBody = card[1].children
+        cardBody[0].textContent = deals[i]._name
+        cardBody[1].textContent = `Price: ${deals[i]._price}`
+        cardBody[2].textContent = `Description: ${deals[i]._description}`
+        console.log(kitchen)
+        
 }
+}
+
+setCardFunc(dealCards)
     
 
 
